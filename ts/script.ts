d15a2b46d6e7301d2861578826a0e51bfc1238fa
hiteacheryouare <82683251+hiteacheryouare@users.noreply.github.com>
2022-04-04 11:32:47 -0400

    revert: :fire: complete restart

--- ts/script.ts
+++
@@ -1,7 +0,0 @@
-// Create a function that displays the current location of the user in the console.
-
-console.log("This is a function that displays the current location of the user in the console.");
-
-function displayLocation() {
-  console.log ("The user is currently located at: " + window.location);
-}
