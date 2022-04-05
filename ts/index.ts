2be549ccba4adc2d88443ef043da4f2fe69d7add
hiteacheryouare <82683251+hiteacheryouare@users.noreply.github.com>
2022-04-04 20:39:25 -0400

    made header zone

--- ts/index.ts
+++ ts/index.ts
@@ -1,1 +1,31 @@
 import '../styles/css/tailwind.css'
+import '../styles/scss/fonts.scss'
+import '../styles/scss/misc.scss'
+import { DEVICE_type } from './UAutility'
+
+const webDev = document.getElementById('webDev');
+const deskDev = document.getElementById('deskDev');
+const frontDev = document.getElementById('frontDev');
+const mobDev = document.getElementById('mobDev');
+
+const detectDeviceAndThenDecideWhatSubtitleToShow = () => {
+    if (DEVICE_type == 'desktop') {
+        mobDev.style.display = 'none';
+    }
+    else if (DEVICE_type == 'mobile') {
+        deskDev.style.display = 'none';
+        webDev.style.display = 'none';
+        frontDev.style.display = 'none';
+    }
+    else {
+        deskDev.style.display = 'none';
+        webDev.style.display = 'none';
+        frontDev.style.display = 'none';
+    }
+};
+
+
+// This code will run on load
+$(document).ready(function () {
+    detectDeviceAndThenDecideWhatSubtitleToShow();
+});
