eddd59dfef7c396265afabd0c2b677e5108c60b5
hiteacheryouare <82683251+hiteacheryouare@users.noreply.github.com>
2022-07-02 23:34:27 -0400

    BASIC VITE SVELTE PROJECT

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,18 @@
-const { resolve } = require('path')
-export default {
-    plugins: [],
-    server: {
-        open: '/index.html',
-        port: 3000,
-    },
-    build: {
-        rollupOptions: {
-            input: {
-                main: resolve(__dirname, 'index.html')
-            },
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [svelte()],
+  build: {
+    rollupOptions: {
+        input: {
+            index: 'index.html',
+            fourOhFour: '404.html',
+        },
+        output: {
+            dir: './docs',
         }
     }
-}
+  }
+})
