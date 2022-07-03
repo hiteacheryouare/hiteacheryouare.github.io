5dc0f1e87df36d3adc78437afc32bfbe06d6c325
hiteacheryouare <82683251+hiteacheryouare@users.noreply.github.com>
2022-07-03 00:02:03 -0400

    added emoji🚧

--- src/components/hero.svelte
+++ src/components/hero.svelte
@@ -13,7 +13,7 @@ import Animals from './animals.svelte'
 <main>
     <div class="bg-primary text-secondary pt-8 pb-96 text-center">
         <h1 class="font-secondary font-bold sm:text-4xl md:text-5xl lg:text-6xl xl:text-8xl">
-            UNDER <br> CONSTRUCTION
+            UNDER <br> CONSTRUCTION 🚧
         </h1>
         <p class="font-secondary font-bold sm:text-2xl md:text-3xl lg:text-3xl xl:text-4xl">
             This Site is Currently Under CONSTRUCTION
