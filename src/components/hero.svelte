32a237c84d96e83862476cc85f9dcf75c72d9ca5
hiteacheryouare <82683251+hiteacheryouare@users.noreply.github.com>
2022-07-04 00:30:05 -0400

    feat: :beers: its broken but its there :)

--- src/components/hero.svelte
+++ src/components/hero.svelte
@@ -4,6 +4,7 @@ import { DEVICE_type } from 'uadetect'
 import Animals from './animals.svelte'
 import HeroTitle from './heroTitle.svelte'
 const pfp = './img/pfp.png'
+const pattern = './img/patternpad.svg'
 if (DEVICE_type === "mobile") {
     document.getElementById('pfp').style.display = "none"
 }
@@ -14,10 +15,12 @@ if (DEVICE_type === "mobile") {
         <HeroTitle />
         <img src={pfp} alt="Ryan Mullin" id="pfp">
     </div>
-    <img src="./img/pattenpad.svg" alt="abstract pattern">
+    <img src={pattern} alt="abstract pattern">
 </main>
 
 <style>
-
+    img {
+        width: 50%;
+    }
 </style>
 
