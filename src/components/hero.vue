a64778fe7a3fc3cb247cf2c4f57b112f8ccc98fe
hiteacheryouare <82683251+hiteacheryouare@users.noreply.github.com>
2022-11-16 13:28:20 -0500

    minor improvements

--- src/components/hero.vue
+++ src/components/hero.vue
@@ -6,8 +6,8 @@
           <path fill="url(#45de2b6b-92d5-4d68-a6a0-9b9b2abad533)" fill-opacity=".3" d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z" />
           <defs>
             <linearGradient id="45de2b6b-92d5-4d68-a6a0-9b9b2abad533" x1="1155.49" x2="-78.208" y1=".177" y2="474.645" gradientUnits="userSpaceOnUse">
-              <stop stop-color="#9089FC" />
-              <stop offset="1" stop-color="#FF80B5" />
+              <stop stop-color="#00ffff" />
+              <stop offset="1" stop-color="#201191" />
             </linearGradient>
           </defs>
         </svg>
@@ -24,7 +24,7 @@
                 </div>
               </div>
               <div>
-                <h1 class="text-4xl font-bold tracking-tight sm:text-center sm:text-6xl">Pushing What's Possible <span class="bg-primary text-secondary rounded"> in every aspect of life.</span></h1>
+                <h1 class="text-4xl font-bold tracking-tight sm:text-center sm:text-6xl">Pushing What's Possible <p class="bg-primary text-secondary rounded"> in every aspect of life.</p></h1>
                 <p class="mt-6 text-lg leading-8 text-gray-600 sm:text-center" >Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.</p>
                 <div class="mt-8 flex gap-x-4 sm:justify-center">
                   <a href="/blog" class="inline-block rounded-lg bg-primary px-4 py-1.5 text-base font-semibold leading-7 text-white shadow-sm ring-1 ring-indigo-600 hover:bg-indigo-700 hover:ring-indigo-700">
