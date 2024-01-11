f8c5cc7e053ebe56f59f42bb074517a3a3f15afc
hiteacheryouare <82683251+hiteacheryouare@users.noreply.github.com>
2024-01-11 11:32:11 -0500

    link to research & misc pages

--- src/utils/navigation.ts
+++ src/utils/navigation.ts
@@ -1,26 +1,30 @@
-interface Navigation {
-	name: string;
-	href: string;
-}
-export const navigation: Navigation[] = [
+export const navigation = [
 	{
 		name: "Home",
 		href: "/"
-	},
-	{
-		name: "Blog",
-		href: "/blog"
 	},
 	{
 		name: "About",
 		href: "/about"
 	},
 	{
+		name: "Blog",
+		href: "/blog"
+	},
+	{
 		name: "Portfolio",
 		href: "/portfolio"
+	},
+	{
+		name: "Research",
+		href: "/research/"
+	},
+	{
+		name: "Misc",
+		href: "/misc/"
 	},
 	{
 		name: "Freelance Web Design",
 		href: "/freelance"
 	}
-]
+] as const
