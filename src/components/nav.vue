74755fd5847df30568b746e5065e8e362b821721
Ryan Mullin <82683251+hiteacheryouare@users.noreply.github.com>
2025-01-23 10:20:33 -0500

    update navigation component styles for improved visual consistency

--- src/components/nav.vue
+++ src/components/nav.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="bg-white" transition:persist>
+  <div class="bg-slate-400" transition:persist>
       <header class="absolute inset-x-0 top-0 z-50">
-        <nav class="flex items-center justify-between p-6 lg:px-8 border-b-2 border-slate-300 dark:border-slate-700" aria-label="Global">
+        <nav class="flex items-center justify-between p-6 lg:px-8 border-b-2 bg-slate-200 dark:bg-slate-700 border-slate-300 dark:border-slate-700" aria-label="Global">
           <div class="flex lg:flex-1">
             <a href="/" class="-m-1.5 p-1.5" style="text-decoration: none;">
               <span class="sr-only">Ryan Enterprises</span>
@@ -20,7 +20,7 @@
         </nav>
         <Dialog as="div" class="lg:hidden" @close="mobileMenuOpen = false" :open="mobileMenuOpen">
           <div class="fixed inset-0 z-50" />
-          <DialogPanel class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+          <DialogPanel class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-slate-500 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
             <div class="flex items-center justify-between">
               <a href="/" class="-m-1.5 p-1.5">
                 <span class="sr-only">Ryan Enterprises</span>
